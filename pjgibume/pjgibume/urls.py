75dfe44672c7a0bc1195ac9ce88436e986ab022a
aann-sunny <kys980123@gmail.com>
2021-08-04 18:12:28 +0900

    test update

---
+++ pjgibume/pjgibume/urls.py
@@ -0,0 +1,53 @@
+"""pjgibume URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+from gibumeapp.views import *
+from testapp.views import *
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home,name="home"),
+    path('mypage', mypage, name = "mypage"),
+    path('detail/',detail,name="detail"),
+
+    # product 페이지 
+    path('product/<str:name>',product,name="product"),
+    path('product/<str:name>/writecomment/', writecomment, name="writecomment"),
+    
+    # 선호도
+    path('like/<str:name>/', like_post, name="like_post"),
+    path('ok/<str:name>/', ok_post, name="ok_post"),
+    path('dislike/<str:name>/', dislike_post, name="dislike_post"),
+
+    # 댓글 삭제
+    path('product/<str:name>/<str:id>/deletecomment', deletecomment, name="deletecomment"),
+
+    # 댓글 좋아요
+    path('product/<str:name>/<str:id>/yesUp', yesUp, name="yesUp"),
+    # 댓글 싫어요
+    path('product/<str:name>/<str:id>/noUp', noUp, name="noUp"),
+
+    path('community/', community, name="community"),
+    path('community_detail/', community_detail, name="community_detail"),
+    path('perfume/', perfume, name = "perfume"),
+    path('perfume/search', search, name="search"),
+    path('education/',education,name="education"),
+    path('account/', include('account.urls')),
+
+    # test 페이지
+    path('test/', test, name="test"),
+    path('form/', form, name="form"),
+]
